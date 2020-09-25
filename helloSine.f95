d15719633ba1a6eff2402d9353466d5e6da4f18e
Hikari-desuyoo <66630725+Hikari-desuyoo@users.noreply.github.com>
2020-09-25 03:27:30 -0300

    Add files via upload
    Tem uma parte meio hardcoded, nao sei como arrumar...

---
+++ helloSine.f95
@@ -0,0 +1,26 @@
+!Feito pela Hikari :)
+program HelloSine
+        !declares angle which will increase 
+        !after each interation of the loop
+        real :: angle=0
+        
+        !declare function modifiers
+        real :: inMod=0.5
+        real :: outMod=40
+        real :: addMod =40
+        
+        !declare len value
+        real :: lenvalue=100
+        
+        !declare string
+        character(len=100) :: string
+        
+        string = repeat("I know fortran 95 :)",100)
+          
+          !loop
+          do while (.true.)
+             angle=angle+0.1
+             seno = sin(inMod*angle)*outMod+addMod
+             write(*,*) string(:int(seno))
+          enddo
+        end program
